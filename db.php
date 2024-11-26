6922f503dddd502ab19bdc30c626f873e25af251
sandy0v0 <be0324ss@gmail.com>
2024-11-26 11:44:55 +0800

    建立一個可以讀取資料庫的類別DB + 一些註解

--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
 
+// 去資料庫建一個db99的資料庫，要複製school的classes，點classes再去操作頁面，copy table to (database.table)，直接執行即可
+
 class DB{
     protected $dsn="mysql:host=localhost;charset=utf8;dbname=db99";
     protected $pdo;
@@ -24,7 +26,7 @@ function dd($array){
     echo "(/pre)";
 }
 
-
+// 你要抓的資料庫為(classes)，所以要注意抓的地方是哪裡
 $DEPT=new DB('classes');
 
 $dept=$DEPT->q("SELECT * FROM classes");
